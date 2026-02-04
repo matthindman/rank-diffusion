e52e03c20d4be3b5905be12542be1eb0e735f58c
matthindman <hindman@gmail.com>
2026-02-04 09:44:12 -0500

    fix smoke test render environment

--- scripts/smoke_test.R
+++ scripts/smoke_test.R
@@ -1,13 +1,13 @@
-source(here::here("R", "init.R"))
-
 rmarkdown::render(
   input = here::here("report", "section_wrapper.Rmd"),
   params = list(child = "analysis/01_data_qc.Rmd"),
-  output_dir = here::here("output", "report")
+  output_dir = here::here("output", "report"),
+  envir = new.env(parent = globalenv())
 )
 
 rmarkdown::render(
   input = here::here("report", "master_report.Rmd"),
   params = list(mode = "dev", run_heavy = FALSE, use_cache = TRUE),
-  output_dir = here::here("output", "report")
+  output_dir = here::here("output", "report"),
+  envir = new.env(parent = globalenv())
 )
